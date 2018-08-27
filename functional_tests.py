6f2d6134391805e558cb3552fe41e242fecf4197
Danielbutler <dbutler@coregroupsales.com>
2018-08-27 12:57:05 -0400

    Updated comments to fit 80 character limit

--- functional_tests.py
+++ functional_tests.py
@@ -12,8 +12,8 @@ class NewVistorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edit has heard about a cool new online to-do app. She goes to check out its
-        # homepage
+        # Edit has heard about a cool new online to-do app. She goes to check
+        # out its homepage
         self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
@@ -22,20 +22,21 @@ class NewVistorTest(unittest.TestCase):
 
         # She is invited to enter a to-do item straight away
 
-        # She types "Buy peacock feathers" into a text box (Edithi's hobby is tying
-        # fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box (Edithi's hobby is
+        # tying fly-fishing lures)
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do lists
 
-        # There is still a text box inviting her to add another item. She enteres
-        # "Use peacock feathers to make a fly" (Edith is very methodical)
+        # There is still a text box inviting her to add another item. She
+        # enteres "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
 
         # The page updates again, and now shows both items on her lists
 
-        # Edit wonders whether the site will remember her list. Then she sees that the
-        # site has generated a unique URL for her -- there is some explanatory text
-        # to that effect.
+        # Edit wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there.
 
