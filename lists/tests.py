6882362c5bebafdc247f659c992f955898bbb1ce
Danielbutler <dbutler@coregroupsales.com>
2018-08-28 12:53:02 -0400

    Moved home.html template to lists namespace

--- lists/tests.py
+++ lists/tests.py
@@ -10,4 +10,4 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'lists\home.html')
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'lists\home.html')
