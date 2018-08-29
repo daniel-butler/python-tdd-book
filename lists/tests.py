74c35770c47da40583ba759033169f3eab3e0960
daniel <dabutler89@gmail.com>
2018-08-28 23:31:52 -0400

    Requests to home page only save to the database when it is a POST

--- lists/tests.py
+++ lists/tests.py
@@ -13,11 +13,20 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, r'lists\home.html')
 
+    # TODO: Code smell: Test is too long?
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, r'lists\home.html')
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from lists.models import Item
 
 
 def home_page(request):
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+
     return render(request, r'lists\home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': new_item_text
     })
