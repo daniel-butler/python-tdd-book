d645ad7f3ac9b9d283ca418ba3e9926f404812e8
daniel <dabutler89@gmail.com>
2018-08-28 21:58:27 -0400

    refactored tests to consolidate table row check

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVistorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edit has heard about a cool new online to-do app. She goes to check
         # out its homepage
@@ -38,10 +43,7 @@ class NewVistorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enteres "Use peacock feathers to make a fly" (Edith is very
@@ -54,10 +56,8 @@ class NewVistorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her lists
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edit wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
