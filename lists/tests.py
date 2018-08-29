704dc797fefc36cbee21a880bc8eb93b143a4de3
daniel <dabutler89@gmail.com>
2018-08-28 21:47:31 -0400

    functional test and views for posting items to lists updated.

--- lists/tests.py
+++ lists/tests.py
@@ -8,6 +8,11 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_uses_home_template(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'lists\home.html')
+        self.assertTemplateUsed(response, r'lists\home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, r'lists\home.html')
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'lists\home.html')
+    return render(request, r'lists\home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
