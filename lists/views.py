2df16c7964724d8ac508b8133c5cae50d76adbe2
daniel <dabutler89@gmail.com>
2018-08-30 22:09:26 -0400

    added list/new url

--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,12 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, r'lists\home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, r'lists\list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
