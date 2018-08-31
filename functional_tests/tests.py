ed0fa0534ef4d39b4e6a6b6faad2d46850fe34c5
daniel <dabutler89@gmail.com>
2018-08-31 15:39:43 -0400

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -79,6 +79,30 @@ class NewVistorTest(LiveServerTestCase):
 
         # Statisfied, she goes back to sleep
 
+    def test_layout_and_styling(self):
+        # Edit goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edit starts a new to-do list
         self.browser.get(self.live_server_url)
