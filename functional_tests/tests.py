9b7a58bc186c9183f3c89d359634ee5771ba6470
daniel <dabutler89@gmail.com>
2018-08-31 17:27:49 -0400

    set STATIC_ROOT in settings and disable admin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -71,13 +71,6 @@ class NewVistorTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Edit wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
         # Statisfied, she goes back to sleep
 
     def test_layout_and_styling(self):
