7411d5817c5f62b059ae812e365d9be45f4ff511
daniel <dabutler89@gmail.com>
2018-09-07 21:11:03 -0400

    form now handles database save

--- lists/views.py
+++ lists/views.py
@@ -14,15 +14,15 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, r'lists/list.html', {'list': list_, 'form': form,})
 
 def new_list(request):
-    form = ItemForm(request.POST)
+    form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/home.html', {"form": form})
