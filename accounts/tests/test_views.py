43c94fe305950f6bc9f19f80180dc7aaa5ed93ec
daniel <dabutler89@gmail.com>
2018-09-12 22:43:37 -0400

    working throught mock chapter

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, call
 
+from accounts.models import Token
 import accounts.views
 
 
@@ -23,3 +24,40 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(subject, 'Your login link for Superlists')
         self.assertEqual(from_email, 'noreply@lists.accounting.equipment')
         self.assertEqual(to_list, ['edith@example.com'])
+
+    @patch('accounts.views.messages')
+    def test_adds_success_message(self, mock_messages):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        expected = "Check your email, we've sent you a link you can use to log in."
+
+        self.assertEqual(
+            mock_messages.success.call_args,
+            call(response.wsgi_request, expected)
+        )
+
+class LoginViewTest(TestCase):
+
+    def test_redirects_to_home_page(self):
+        response = self.client.get('/accounts/login?token=abc123')
+        self.assertRedirects(response, '/')
+
+    def test_creates_token_associated_with_email(self):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'edith@example.com')
+
+    @patch("accounts.views.send_mail")
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        token = Token.objects.first()
+        expected_url = f"http://testserver/accounts/login?token={token.uid}"
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
+from django.contrib import messages
 
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
     send_mail(
         'Your login link for Superlists',
-        'body text bc',
+        'Use this link to log in',
         'noreply@lists.accounting.equipment',
         [email]
     )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
+
+def login(request):
     return redirect('/')
