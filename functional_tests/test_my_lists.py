87522b0e35ce5015c6bd174030481887f729ce59
daniel <dabutler89@gmail.com>
2018-09-23 22:13:13 -0400

    finished my lists without commplete isolation

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -61,6 +61,6 @@ class MyListsTest(FunctionalTest):
         # She logs out. The "My lists" option disappears
         self.browser.find_element_by_link_text('Log out').click()
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_link_text('My lists'),
+            self.browser.find_elements_by_link_text('My lists'),
             []
         ))
