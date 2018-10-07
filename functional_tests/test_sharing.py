7489ab6a8db049e568b0721de9776cc918b7f5d2
Daniel Butler <daniel@dbutlercpa.com>
2018-10-07 16:32:43 -0400

    Create Page objects for list pages, use in sharing FT

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+
 from .base import FunctionalTest
+from .list_page import ListPage
+from .my_lists_page import MyListsPage
 
 
 def quit_if_possible(browser):
@@ -24,13 +27,36 @@ class SharingTest(FunctionalTest):
         # Edith goes to the home page and starts a list
         self.browser = edith_browser
         self.browser.get(self.live_server_url)
-        self.add_list_item('Get help')
+        list_page = ListPage(self).add_list_item('Get help')
 
         # She notices a "Share this list" option
-        share_box = self.browser.find_element_by_css_selector(
-            'input[name=sharee]'
-        )
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.com'
         )
+
+        # She shares her list.
+        # The page updates to say that it's shared with Oniciferous
+        list_page.share_list_with('oniciferous@example.com')
+
+        # Oniciferous now goes to the lists page with his browser
+        self.browser = oni_broswer
+        MyListsPage(self).go_to_my_lists_page()
+
+        # He sees Edith's list in there!
+        self.browser.find_element_by_link_text('Get help').click()
+
+        # On the list page, Oniciferous can see says that it's Edith's list
+        self.wait_for(lambda: self.assertEqual(
+            list_page.get_list_ownerr(),
+            'edith@example.com'
+        ))
+
+        # He adds an item to the list
+        list_page.add_list_item('Hi Edith!')
+
+        # When Edith refreshes the page, she sees  Oniciferous's addition
+        self.browser = edith_browser
+        self.browser.refresh()
+        list_page.wait_for_row_in_list_table('Hi Edith!', 2)
