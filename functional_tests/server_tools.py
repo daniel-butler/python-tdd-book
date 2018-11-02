25790016a414538e73a6ee87c9d36220f899ea76
Daniel Butler <daniel@dbutlercpa.com>
2018-11-02 17:23:25 -0400

    pep8

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -5,14 +5,17 @@ from fabric.context_managers import settings, shell_env
 def _get_manage_dot_py(host):
     return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
 
+
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
     with settings(host_string=f'daniel@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
+
 def _get_server_env_vars(host):
     env_lines = run(f'cat ~/sites/{host}/.env').splitlines()
     return dict(l.split('=') for l in env_lines if l)
+
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
