929d2156e2db16ca8e63e9fdd898e6d27995efba
Daniel Butler <daniel@dbutlercpa.com>
2018-11-02 21:10:54 -0400

    updated jinja custom function

--- deploy_tools/key.py
+++ deploy_tools/key.py
@@ -1,18 +1,10 @@
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-
 from jinja2 import Environment, FileSystemLoader
 from django.utils.crypto import get_random_string
 
 
-def create_secret_key():
+def create_django_secret_key():
     return get_random_string(50)
 
 
-class FilterModule(object):
-    """ adds generate random secret key filter """
-    def filters(self):
-        return {
-            # filter map
-            'create_secret_key': create_secret_key
-        }
+env = Environment(loader=FileSystemLoader('/'))
+env.globals['create_django_secret_key'] = create_django_secret_key
